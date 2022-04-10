1244698fe8dd84c2aa54a621609b2df4429e9193
Riddhiman <shubro17@outlook.com>
2022-04-10 16:25:42 +0530

    created function to resetPwd

--- routes/userRouter.router.js
+++ routes/userRouter.router.js
@@ -53,4 +53,21 @@ userRouter.post('/createUser',async(req,res)=>{
     })
 })
 
+userRouter.post('/userResetPwd',async (req,res)=>{
+    const query = {USERID:req.body.USERID}
+    const user = await USER.exists(query)
+    if(user){
+        const newuser = await USER.findOneAndUpdate(query,{
+            PWD:req.body.PWD
+        })
+        res.status(200).send({
+            Message:'USER Updated'
+        })
+    }else{
+        res.status(404).send({
+            Message:'USER does not Exist'
+        })
+    }
+})
+
 module.exports = userRouter;
