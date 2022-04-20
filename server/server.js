b10816f39249cb31ec052ad29134530cfac5b86d
Riddhiman <shubro17@outlook.com>
2022-04-21 00:23:09 +0530

    Linking Live Scores to Mongo

--- server/server.js
+++ server/server.js
@@ -3,19 +3,61 @@ const app = require('./app');
 const server = http.createServer(app);
 const port = process.env.PORT||3000 // to be upgraded for .env files later
 const {Server} = require('socket.io');
+const tabletennis = require('../models/tabletennis.model')
+
 const io = new Server(server)
-io.on("connect",(socket)=>{
-    console.log(socket.id);
-    let roomname
+//We are currently doing it for singles of raquet games(example: tabletennis)
+io.on("connect",async (socket)=>{
+    const socket_id = socket.id
     socket.on('join-room',(obj)=>{
-        roomname = obj.roomid;
+        //entity can have values - USER/LIVE-MAINTAINER/ADMIN/EVENT-MANAGER
+        //entityID means either USERID, LIVE-MAINTAINER_ID etc..
+        console.log(obj);
+        const entity = obj.entity
+        const entity_ID = obj.entity_ID
+        let roomname = obj.MATCHID;
+        console.log(roomname);
+        let sport = obj.sport
         socket.join(roomname);
-        io.to(roomname).emit('Joined Match')
-    })
-    socket.on('update-score',(obj)=>{
-        io.to(roomname).emit('new-score',{
-            newscore1:obj.score1,
-            newscore2:obj.score2
+        socket.to(socket_id).emit('joined-room',{
+            Message:roomname
+        })
+        if(entity=='LIVE-MAINTAINER'){
+            io.to(roomname).emit('joined-match',{
+                Message:`LIVE-MAINTAINER: ${entity_ID} has joined the match`
+            })
+        }
+        else if(entity=='USER'){
+            io.to(roomname).emit('joined-match',{
+                Message:`USER: ${entity_ID} has joined the match`
+            })
+        }
+        //update-score event for tabletennis
+        socket.on('update-score',async (obj)=>{
+            const pl1score = obj.PLAYER_1_SCORE
+            const pl2score = obj.PLAYER_2_SCORE
+            try{
+                const match = await tabletennis.findOneAndUpdate({
+                    MATCHID:roomname
+                },{
+                    PLAYER1_SCORE:pl1score,
+                    PLAYER2_SCORE:pl2score
+                })
+                if(match){
+                    io.to(roomname).emit('score-updated',{
+                        PLAYER_1_SCORE:pl1score,
+                        PLAYER_2_SCORE:pl2score
+                    })    
+                }
+                else{
+                    io.to(roomname).emit('ERROR',{
+                        Message:'Match not found in db'
+                    })    
+                }
+            }catch(err){
+                console.log(err);
+                socket.to(socket_id).emit('ERROR')
+            }
         })
     })
 })
