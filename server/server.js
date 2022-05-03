4484699e0c0c85ce7c8c3ca16ff4c403ab7cd1d4
Riddhiman <shubro17@outlook.com>
2022-05-03 23:09:28 +0530

    Commit3

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const tabletennis = require('../models/tabletennis.model')
 const io = new Server(server)
 //We are currently doing it for singles of raquet games(example: tabletennis)
 io.on("connection",async (socket)=>{
+    console.log(socket.id);
     const socket_id = socket.id
     socket.on('join-room',(obj)=>{
         //entity can have values - USER/LIVE-MAINTAINER/ADMIN/EVENT-MANAGER
