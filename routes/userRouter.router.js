8fcb832b04600bb304ac6b457dee1ae2304ad41a
Riddhiman <shubro17@outlook.com>
2023-01-21 19:28:18 +0530

    Timing Changed,Payments in Live

--- routes/userRouter.router.js
+++ routes/userRouter.router.js
@@ -21,8 +21,8 @@ const timings = require('../models/timings.mongo')
 const matchtiming = require('../models/matchtimings.mongo')
 const csvgen = require('json2csv').Parser
 const rzp_instance = new razorpay({
-    key_id:'rzp_test_MKSizWXlqa0LsE',
-   key_secret:'000II2IEeqG4k6rD07W3HBDX'
+    key_id:'rzp_live_4JAecB352A9wtt',
+   key_secret:'UhkUVQq781FVniExGipwVCwi'
 })
 const s3 = new S3({
     region:'ap-south-1',
