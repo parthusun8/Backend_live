dfb78fb7d8bb7d8b7e048f4ac659e20ceab30d92
Parth Sundarka <77690791+parthusun8@users.noreply.github.com>
2023-03-21 23:35:26 +0530

    Added Scoring Schema

--- models/matches.mongo.js
+++ models/matches.mongo.js
@@ -1,106 +1,105 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const bdmatch = new mongoose.Schema({
-    MATCHID:{
-        type:String,
-        required:true
+  MATCHID: {
+    type: String,
+    required: true,
+  },
+  TOURNAMENT_ID: {
+    type: String,
+    required: true,
+  },
+  TOURNAMENT_NAME: {
+    type: String,
+  },
+  CATEGORY: {
+    type: String,
+  },
+  PLAYER1: {
+    type: String, //Array of userids for doubles. If single player just give one player in the array,
+    default: "Not Yet Assigned",
+  },
+  PLAYER2: {
+    type: String,
+    default: "Not Yet Assigned", //Array of userids for doubles. If single player just give one player in the array
+  },
+  PLAYER1_SCORE: {
+    set1: {
+      type: Number,
+      default: 0,
     },
-    TOURNAMENT_ID:{
-        type:String,
-        required:true
+    set2: {
+      type: Number,
+      default: 0,
     },
-    TOURNAMENT_NAME:{
-        type:String
+    set3: {
+      type: Number,
+      default: 0,
     },
-    CATEGORY:{
-        type:String
+    set4: {
+      type: Number,
+      default: 0,
     },
-    PLAYER1:{
-        type:String,//Array of userids for doubles. If single player just give one player in the array,
-        default:"Not Yet Assigned"
+    set5: {
+      type: Number,
+      default: 0,
     },
-    PLAYER2:{
-        type:String,
-        default:"Not Yet Assigned"//Array of userids for doubles. If single player just give one player in the array
+  },
+  PLAYER2_SCORE: {
+    set1: {
+      type: Number,
+      default: 0,
     },
-    PLAYER1_SCORE:{
-        set1:{
-            type:Number,
-            default:0
-        },
-        set2:{
-            type:Number,
-            default:0
-        },
-        set3:{
-            type:Number,
-            default:0
-        },
-        set4:{
-            type:Number,
-            default:0
-        },
-        set5:{
-            type:Number,
-            default:0
-        }
-  
+    set2: {
+      type: Number,
+      default: 0,
     },
-    PLAYER2_SCORE:{
-        set1:{
-            type:Number,
-            default:0
-        },
-        set2:{
-            type:Number,
-            default:0
-        },
-        set3:{
-            type:Number,
-            default:0
-        },
-        set4:{
-            type:Number,
-            default:0
-        },
-        set5:{
-            type:Number,
-            default:0
-        }
+    set3: {
+      type: Number,
+      default: 0,
     },
-    SCORER:{
-        type:String,
-        default:"Not Assigned"
+    set4: {
+      type: Number,
+      default: 0,
     },
-    ROUND_NO:{
-        type:Number
+    set5: {
+      type: Number,
+      default: 0,
     },
-    TIME: [Number],
-    NEXT_MATCH_ID:{
-        type:String
-    },
-    NEXT_MATCH_PLAYER_SPOT:{
-      type:Number
-    },
-    winner_id:{
-        type:String
-    },
-    winner_name:{
-        type:String
-    },
-    completion_status:{
-        type:String,
-        default:"Not Complete"
-    }        
-  })
-  const tourna = new mongoose.Schema({
-    TOURNAMENT_ID:{
-        type:String
-    },
-    TOURNAMENT_NAME:{
-        type:String
-    },
-    MATCHES:{
-        type:[bdmatch]
-    }
-  })
-  module.exports = mongoose.model('matches', tourna);
+  },
+  SCORER: {
+    type: String,
+    default: "Not Assigned",
+  },
+  ROUND_NO: {
+    type: Number,
+  },
+  TIME: [Number],
+  NEXT_MATCH_ID: {
+    type: String,
+  },
+  NEXT_MATCH_PLAYER_SPOT: {
+    type: Number,
+  },
+  winner_id: {
+    type: String,
+  },
+  winner_name: {
+    type: String,
+  },
+  completion_status: {
+    type: String,
+    default: "Not Complete",
+  },
+});
+const tourna = new mongoose.Schema({
+  TOURNAMENT_ID: {
+    type: String,
+  },
+  TOURNAMENT_NAME: {
+    type: String,
+  },
+  MATCHES: {
+    type: [bdmatch],
+  },
+});
+module.exports = mongoose.model("matches", tourna);
--- models/team.model.js
+++ models/team.model.js
@@ -1,13 +1,52 @@
 const mongoose = require('mongoose');
+const player = new mongoose.Schema({
+    NAME : {
+        type:String,
+        required : true,
+    }, 
+    USERID : {
+        type:String,
+        required : true,
+    }, 
+    RUNS : { //runs given by player while balling
+        type: Number,
+        default : 0,
+    }, 
+    WICKETS : {
+        type: Number,
+        default : 0,
+    },
+    SCORE : { //runs scored by player while batting
+        type: Number,
+    }
+});
+const substitutes = new mongoose.Schema({
+    NAME : {
+        type:String,
+        required : true,
+    }, 
+    USERID : {
+        type:String,
+        required : true,
+    }, 
+    RUNS : {
+        type: Number,
+        default : 0,
+    }, 
+    WICKETS : {
+        type: Number,
+        default : 0,
+    },
+});
 const cricketTeam = new mongoose.Schema({
     TOURNAMENT_ID: {
         type: String,
     },
     PLAYERS : {
-        type:[Object],
+        type:[player],
     },
     SUBSTITUTE : {
-        type:[Object],
+        type:[substitutes],
     },
     TEAM_NAME:{
         type:String,
