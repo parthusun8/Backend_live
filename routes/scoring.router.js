e8b4b0ba66fd70036d64fc5f056832d477b90c99
Parth Sundarka <77690791+parthusun8@users.noreply.github.com>
2023-03-26 14:49:23 +0530

    Added Score Card Page

--- routes/scoring.router.js
+++ routes/scoring.router.js
@@ -1161,9 +1161,9 @@ ScoringRouter.get("/cricketFixtures", async (req, res) =>{
     console.log(checkExists.TOTAL_MATCHES);
     if(checkExists){
       res.render("cricket_fixture", {
-        TOURNEY_ID: req.query.TOURNAMENT_ID,
-        no_of_bracs: 64,
-        USERID: req.query.USERID,
+        TOURNAMENT_ID :  req.query.TOURNAMENT_ID,
+        no_of_bracs: checkExists.TOTAL_MATCHES + 1,
+        allData : JSON.stringify(checkExists),
       });
     }
     
@@ -1172,5 +1172,145 @@ ScoringRouter.get("/cricketFixtures", async (req, res) =>{
     console.log(e);
     res.status(200).render("version");
   }
+});
+
+ScoringRouter.get("/ScoreCardOnFixtures", async (req, res) => {
+  var checkExists = {};
+    try {
+      checkExists = await score.findOne({
+        TOURNAMENT_ID: req.query.TOURNAMENT_ID,
+      });
+      if (checkExists) {
+        var returnVal = {
+          scoreCard: [
+            {
+              TeamName: "",
+              TeamTotal: 0,
+              TeamWickets: 0,
+              TeamOvers: 0,
+            },
+            {
+              TeamName: "",
+              TeamTotal: 0,
+              TeamWickets: 0,
+              TeamOvers: 0,
+            },
+          ],
+          WinnerTeam: "",
+        };
+  
+        returnVal.scoreCard[0].TeamName =
+          checkExists.MATCHES[
+            req.query.match_no
+          ].TEAMS[1].TEAM_NAME;
+        returnVal.scoreCard[0].TeamTotal =
+          checkExists.MATCHES[
+            req.query.match_no
+          ].INNING[0].BATTING_DETAILS.SCORE;
+        returnVal.scoreCard[0].TeamWickets =
+          checkExists.MATCHES[req.query.match_no].INNING[0].WICKETS;
+        returnVal.scoreCard[0].TeamOvers =
+          checkExists.MATCHES[
+            req.query.match_no
+          ].INNING[0].OVERS_DONE;
+  
+        var extraOvers =
+          checkExists.MATCHES[
+            req.query.match_no
+          ].INNING[0].CURRENT_OVER.split("-");
+        var countExtra = 0;
+        for (var i = 0; i < extraOvers.length - 1; i += 1) {
+          if (
+            extraOvers[i] == "0" ||
+            extraOvers[i] == "1" ||
+            extraOvers[i] == "2" ||
+            extraOvers[i] == "3" ||
+            extraOvers[i] == "4" ||
+            extraOvers[i] == "5" ||
+            extraOvers[i] == "6" ||
+            extraOvers[i] == "Bye"
+          ) {
+            countExtra += 0.1;
+          }
+        }
+        if (countExtra < 0.6) {
+          returnVal.scoreCard[0].TeamOvers += countExtra;
+        } else {
+          returnVal.scoreCard[0].TeamOvers += 1;
+        }
+  
+        returnVal.scoreCard[1].TeamName =
+          checkExists.MATCHES[
+            req.query.match_no
+          ].TEAMS[0].TEAM_NAME;
+        returnVal.scoreCard[1].TeamTotal =
+          checkExists.MATCHES[
+            req.query.match_no
+          ].INNING[1].BATTING_DETAILS.SCORE;
+        returnVal.scoreCard[1].TeamWickets =
+          checkExists.MATCHES[req.query.match_no].INNING[1].WICKETS;
+        returnVal.scoreCard[1].TeamOvers =
+          checkExists.MATCHES[
+            req.query.match_no
+          ].INNING[1].OVERS_DONE;
+  
+        var extraOvers =
+          checkExists.MATCHES[
+            req.query.match_no
+          ].INNING[1].CURRENT_OVER.split("-");
+        var countExtra = 0;
+        for (var i = 0; i < extraOvers.length - 1; i += 1) {
+          if (
+            extraOvers[i] == "0" ||
+            extraOvers[i] == "1" ||
+            extraOvers[i] == "2" ||
+            extraOvers[i] == "3" ||
+            extraOvers[i] == "4" ||
+            extraOvers[i] == "5" ||
+            extraOvers[i] == "6" ||
+            extraOvers[i] == "Bye"
+          ) {
+            countExtra += 0.1;
+          }
+        }
+  
+        if (countExtra < 0.6) {
+          returnVal.scoreCard[1].TeamOvers += countExtra;
+        } else {
+          returnVal.scoreCard[1].TeamOvers += 1;
+        }
+  
+        var firstInningScore =
+          checkExists.MATCHES[req.query.match_no].INNING[0]
+            .BATTING_DETAILS.SCORE;
+        var secondInningScore =
+          checkExists.MATCHES[req.query.match_no].INNING[1]
+            .BATTING_DETAILS.SCORE;
+  
+        if (firstInningScore > secondInningScore) {
+          returnVal.WinnerTeam =
+            checkExists.MATCHES[
+              req.query.match_no
+            ].TEAMS[1].TEAM_NAME;
+        } else {
+          returnVal.WinnerTeam =
+            checkExists.MATCHES[
+              req.query.match_no
+            ].TEAMS[0].TEAM_NAME;
+        }
+  
+        res.render("scorecard", {data : JSON.stringify(returnVal)});
+      } else {
+        res.status(200).send("Wrong Tournament Id");
+      }
+    } catch (e) {
+      console.log("Error Occured");
+      console.log(e);
+      await score.updateOne(
+        { TOURNAMENT_ID: req.query.TOURNAMENT_ID },
+        checkExists
+      );
+      res.status(400).send("Error Occured");
+    }
 });
 module.exports = ScoringRouter;
